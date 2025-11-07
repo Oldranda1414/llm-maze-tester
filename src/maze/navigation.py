8a63c5dfca6b914e37868401625abf309f1dd3bf
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-07 11:35:15 +0100

    minor refactor

--- src/maze/navigation.py
+++ src/maze/navigation.py
@@ -11,21 +11,21 @@ class ConnectionList:
         self.horizontal = horizontal
 
     def connected(self, a: Coordinate, b: Coordinate) -> bool:
-        """Return True if a and b are directly connected."""
+        """Return True if a and b are adiacent and connected."""
         (r1, c1), (r2, c2) = a, b
         dr, dc = r2 - r1, c2 - c1
 
         if abs(dr) + abs(dc) != 1:
             return False
 
-        # TODO refactor this to use direction()
-        if dr == 1:
+        d = direction(a,b)
+        if d == Direction.SOUTH:
             return self.vertical[r1][c1]
-        if dr == -1:
+        if d == Direction.NORTH:
             return self.vertical[r2][c2]
-        if dc == 1:
+        if d == Direction.EAST:
             return self.horizontal[r1][c1]
-        if dc == -1:
+        if d == Direction.WEST:
             return self.horizontal[r2][c2]
         return False
 
