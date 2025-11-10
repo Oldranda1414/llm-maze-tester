d4aa8f49dc673bd374f1c5be2e3fec7903d50337
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-10 16:27:59 +0100

    try to improve warning

--- maze_solver/src/prompt/warning.py
+++ maze_solver/src/prompt/warning.py
@@ -1,11 +1,20 @@
 illegal_answer_warning_text = """
 Your last answer did not end with a valid direction.
 
-Remember that your answers must end by providing the direction you want to move to. The valid directions you can provide are N for north, E for east, S for south, W for west.
+Remember that your answers must end by providing the direction you want to move to.
+
+The valid directions you can provide are N for north, E for east, S for south, W for west.
+
+The last character in your response must be one of the valid directions, including eventual punctuation, so remove it from your last phrase please
 
 As an example if you want to step towards north the last character in your answer would be: N
-"""
 
+Here are some examples of invalid last phrases:
+
+- I want to step north
+- I want to step N!
+- I want to step north!
+"""
 
 illegal_direction_warning_text = """
 You walk directly into a wall, hitting your nose. Place more attention on the direcions that are available to you!
