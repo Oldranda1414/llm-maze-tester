35580875a707d4a06237f753eb016164e15f328c
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-10 17:07:08 +0100

    improve initial prompt

--- maze_solver/src/prompt/prompts.py
+++ maze_solver/src/prompt/prompts.py
@@ -3,11 +3,12 @@ from string import Template
 preamble = Template(
 """
 You are inside a maze. You have a compass with you. The maze is well lit so you can see in all directions how long the corridors are.
-Both the starting cell and the exit cell are on the border of the grid.
-Your goal is to find the maze exit.
+Just like a classic maze the exit is on the border. You could be anywhere in the maze as your starting position.
+Your goal is to find the maze exit. Once you reach it you must step out of the maze with one last move.
 
 Tell me which direction you would like to go to. Provide your answer in the form of the initial of the cardinal direction you wish to take a step forwards to.
 The possible directions are N for north, E for east, S for south, W for west.
+
 
 You are advised to think out loud about what your next move should be, but remember that the last thing you write must be a valid direction.
 
