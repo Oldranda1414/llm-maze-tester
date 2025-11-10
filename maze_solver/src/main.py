e156a233baf610e4ae9dfbfd491ae712a4c32e4b
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-10 13:09:11 +0100

    trying to make first run work

--- maze_solver/src/main.py
+++ maze_solver/src/main.py
@@ -11,25 +11,21 @@ def main():
         results_path = f"results/{model_name}"
         os.makedirs(results_path, exist_ok=True)
         for maze_size in [2,3,4,5]:
-            solved = False
             max_steps = maze_size * maze_size
-            while not solved:
-                maze_solver = MazeSolver(model_name=model_name, maze_size=maze_size)
-                step = 0
-                while not maze_solver.solved() and step < max_steps:
-                    try:
-                        maze_solver.step()
-                        step += 1
-                    except Exception as e:
-                        print("exception occurred...")
-                        print(e)
-                        raise e
-                        break
-                if maze_solver.solved():
-                    print("maze solved!")
-                else:
-                    print("maze not solved...")
-                maze_solver.save_run(f"{results_path}/{maze_size}.yaml")
+            maze_solver = MazeSolver(model_name=model_name, maze_size=maze_size)
+            step = 0
+            while not maze_solver.solved() and step < max_steps:
+                try:
+                    maze_solver.step()
+                    step += 1
+                except Exception as e:
+                    print("exception occurred...")
+                    #raise e
+            if maze_solver.solved():
+                print("maze solved!")
+            else:
+                print("maze not solved...")
+            maze_solver.save_run(f"{results_path}/{maze_size}.yaml")
 
 if __name__ == "__main__":
     main()
