8d32325c2636c55a4f1f2a804a9970d9315ad914
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-10 13:21:31 +0100

    improve solver

--- maze_solver/src/main.py
+++ maze_solver/src/main.py
@@ -12,7 +12,7 @@ def main():
         os.makedirs(results_path, exist_ok=True)
         for maze_size in [2,3,4,5]:
             max_steps = maze_size * maze_size
-            maze_solver = MazeSolver(model_name=model_name, maze_size=maze_size)
+            maze_solver = MazeSolver(model_name=model_name, maze_size=maze_size, quiet=True)
             step = 0
             while not maze_solver.solved() and step < max_steps:
                 try:
