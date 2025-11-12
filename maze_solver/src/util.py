6432e2c2f58b807890534a55e3a7a5c8dfbdde97
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-12 17:52:27 +0100

    improve logging

--- maze_solver/src/util.py
+++ maze_solver/src/util.py
@@ -1,6 +1,6 @@
 def seconds_to_padded_time(seconds: float) -> str:
     hours = int(seconds // 3600)
     minutes = int((seconds % 3600) // 60)
-    remaining_seconds = seconds % 60
+    remaining_seconds = round(seconds % 60)
     
-    return f"{hours:02d}:{minutes:02d}:{remaining_seconds:05.2f}"
+    return f"{hours:02d}:{minutes:02d}:{remaining_seconds:02d}"
