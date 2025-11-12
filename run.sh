70b45ca1b820fae2d347cc28f6bd121e6a0cde09
Oldranda1414 <leonardo.randacio@gmail.com>
2025-11-12 11:40:47 +0100

    improve run logging

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-mkdir -p results/logs
-uv --project maze_solver run maze_solver/src/main.py > "results/logs/$(date +%Y-%m-%d).log"
+uv --project maze_solver run maze_solver/src/main.py
 
