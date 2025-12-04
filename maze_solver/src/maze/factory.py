c252a41f34e84a4117ffd811987cfb6557939dd1
Oldranda1414 <leonardo.randacio@gmail.com>
2025-12-04 15:40:14 +0100

    fix generation filter not working

--- maze_solver/src/maze/factory.py
+++ maze_solver/src/maze/factory.py
@@ -1,7 +1,6 @@
 from typing import Callable
-import random
 
-from maze import Maze, generate_start, generate_target
+from maze import Maze
 from maze.dataset import MazeDataset
 from maze.lattice_maze import LatticeMaze
 from maze.core.coordinate import Coordinate
@@ -16,13 +15,14 @@ def create_dataset(n_mazes: int = 1, maze_size: int = 5, sight_depth: int = 2, s
     return MazeDataset("dataset", n_mazes, maze_size, sight_depth, seed, maze_filter)
 
 def create_maze(size: int = 6, start: Coordinate | None = None, target: Coordinate | None = None, sight_depth: int = 2, seed: int = 42, maze_filter: Callable[[Maze], bool] | None = _is_non_trivial) -> Maze:
-    rng = random.Random(seed)
     random_maze = create_dataset(n_mazes=1, maze_size=size, sight_depth=sight_depth, seed=seed, maze_filter=maze_filter).mazes[0]
-    if target is None:
-        target = generate_target(size, rng)
-    if start is None:
-        start = generate_start(size, target, rng)
-    return LatticeMaze(random_maze.connection_list, size, start, target, sight_depth)
+    final_target = random_maze.target
+    final_start = random_maze.start
+    if target:
+        final_target = target
+    if start:
+        final_start = start
+    return LatticeMaze(random_maze.connection_list, size, final_start, final_target, sight_depth)
 
 def maze_from_yaml(yaml_str: str) -> Maze:
     return LatticeMaze.from_yaml(yaml_str)
