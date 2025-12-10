c3b80f1a48225cbd42213e22243d52458e7ad745
Oldranda1414 <leonardo.randacio@gmail.com>
2025-12-10 12:07:08 +0100

    improve prompt token test

--- maze_solver/src/prompt_token_test.py
+++ maze_solver/src/prompt_token_test.py
@@ -3,9 +3,15 @@ from llm.model import Model
 def main():
     model = Model("llama3")
     prompt = "something"
-    while(len(prompt) != 100000):
-        model.ask(prompt)
-        prompt += prompt
+    try:
+        while(len(prompt) != 100000):
+            model.ask(prompt)
+            prompt += prompt
+    except Exception:
+        print(len(prompt))
+
+    model = Model("qwen3")
+    model.ask("something")
 
 if __name__ == "__main__":
     main()
