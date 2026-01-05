876f4422dc22f7b2ea6c210d0dc9ec901f7767e7
Oldranda1414 <leonardo.randacio@gmail.com>
2026-01-05 11:56:03 +0100

    improve stats cli command

--- maze_solver/src/stats_main.py
+++ maze_solver/src/stats_main.py
@@ -1,12 +1,53 @@
-from experiment import Experiment
 import sys
+import os
+from typing import Callable
+
+from experiment import Experiment
+
+def help():
+    print("If first argument is not a command, it must be a valid experiment date")
+    print("Possible commands:")
+    print("   last: print stats of last experiment")
+    print("   list: list available experiment dates")
+    print("   help: print help message")
+
+def last():
+    default(experiment_list()[-1])
+
+def print_experiment_list():
+    print('Available experiments:')
+    print("\n".join(experiment_list()))
+
+def default(date: str):
+    if date not in experiment_list():
+        print(f"Provided date ({date}) is not a valid experiment date")
+        print_experiment_list()
+        return
+    Experiment(date).print_experiment_stats()
+
+def commands() -> dict[str, Callable]:
+    return {
+        "help": help,
+        "last": last,
+        "list": print_experiment_list
+    }
+
+def experiment_list():
+    experiments = [d for d in os.listdir('results') if os.path.isdir(os.path.join('results', d))]
+    experiments.remove("logs")
+    experiments.sort()
+    return experiments if experiments else []
 
 def main() -> None:
-    if len(sys.argv) < 2:
-        raise ValueError("No argument provided. Please provide the date of the experiment.")
+    if len(sys.argv) == 1:
+        help()
+        return
 
-    date = sys.argv[1]
-    Experiment(date).print_experiment_stats()
+    arg = sys.argv[1]
+    if arg in commands().keys():
+        commands()[arg]()
+    else:
+        default(arg)
 
 if __name__ == "__main__":
     main()
