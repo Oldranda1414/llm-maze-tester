d2b00045cb4b1958e9e6e0a4c78b971295a4150e
Oldranda1414 <leonardo.randacio@gmail.com>
2026-01-23 12:44:18 +0100

    remove comments

--- maze_solver/src/test/colored_maze.py
+++ maze_solver/src/test/colored_maze.py
@@ -1,10 +1,9 @@
-from maze.colored_cell import CellColor, ColoredCell, random_colored_cells
+from maze.colored_cell import random_colored_cells
 from maze.core.direction import Direction
 from maze.factory import create_maze
 
 
 def run():
-    # colored_cells = [ColoredCell((1, 1), CellColor.ORANGE)]
     colored_cells = random_colored_cells(1, 6, 10)
     maze = create_maze(colored_cells=colored_cells[0], start=(1, 1))
     maze.move(Direction.WEST)
