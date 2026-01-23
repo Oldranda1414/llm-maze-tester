6c2f27c719bc38504bae575969179af1a3a53cb2
Oldranda1414 <leonardo.randacio@gmail.com>
2026-01-23 11:41:34 +0100

    implement random colored cells

--- maze_solver/src/maze/colored_cell.py
+++ maze_solver/src/maze/colored_cell.py
@@ -1,3 +1,4 @@
+import random
 from enum import Enum
 from dataclasses import dataclass
 
@@ -15,6 +16,9 @@ class CellColor(Enum):
     MAGENTA = "#FF00FF"
     GRAY = "#808080"
     ORANGE = "#FFA500"
+
+    def to_hex(self) -> str:
+        return self.value
 
 
 @dataclass
@@ -36,3 +40,45 @@ class ColoredCell:
             coordinate=Coordinate(data["coordinate"]),
             color=CellColor[data["color"]],
         )
+
+
+def random_colored_cells(
+    n_mazes: int, maze_size: int, n_colored_cells: int, seed: int = 42
+) -> list[list[ColoredCell]]:
+    rng = random.Random(seed)
+    if n_colored_cells >= len(CellColor):
+        raise ValueError(
+            f"n_colored_cells cannot be bigger then the available number of colors ({len(CellColor)})"
+        )
+    colored_cells: list[list[ColoredCell]] = []
+    for _ in range(n_mazes):
+        colored_cells.append(
+            [
+                ColoredCell(coord, color)
+                for coord in _unique_random_coordinates(maze_size, n_colored_cells, rng)
+                for color in rng.sample(set(CellColor), n_colored_cells)
+            ]
+        )
+
+    return colored_cells
+
+
+def _unique_random_coordinates(
+    max_val: int, n: int, rng: random.Random
+) -> list[Coordinate]:
+    max_possible_tuples = max_val * max_val
+    if n > max_possible_tuples:
+        raise ValueError(
+            f"Cannot generate {n} unique tuples. "
+            f"Maximum possible with max_val={max_val} is {max_possible_tuples}"
+        )
+
+    # Use a set to ensure uniqueness
+    unique_coordinates = set()
+
+    # Generate tuples until we have n unique ones
+    while len(unique_coordinates) < n:
+        coordinate_val = (rng.randrange(max_val), rng.randrange(max_val))
+        unique_coordinates.add(coordinate_val)
+
+    return list(unique_coordinates)
