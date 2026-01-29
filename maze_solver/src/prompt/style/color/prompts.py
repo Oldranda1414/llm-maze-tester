d989157edd7db6f3bde51adce216138d5d240704
Oldranda1414 <leonardo.randacio@gmail.com>
2026-01-29 15:04:59 +0100

    fix prompt typo

--- maze_solver/src/prompt/style/color/prompts.py
+++ maze_solver/src/prompt/style/color/prompts.py
@@ -5,9 +5,9 @@ color_explanation = """
 Some of the cells of the maze have a colored floor tiles. Every color appears only once. Keep track of the ones you find, as they can help you navigate the maze.
 """
 
-current_floor_color = Template("""
-The floor you are currently standing on is colored ${color}.
-""")
+current_floor_color = Template(
+    "The floor you are currently standing on is colored ${color}."
+)
 
 no_current_floor_color = (
     "The floor you are currently standing on has no distinctive color."
